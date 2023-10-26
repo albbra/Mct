dd1a16ace141d6f2df3fefcdd15c0f239f0019f9
albbra <122829039+albbra@users.noreply.github.com>
2023-10-26 11:29:24 +0200

    Add files via upload

--- basic/main.s
+++ basic/main.s
@@ -54,14 +54,20 @@ main PROC
 		
 loop	
 
-	ldr R8, =0x54 ; Lade 54h in Register
-	add R8, #1 ; Inkrementiere Register
-	lsl R8, #1 ; Logisches Schieben um ein Bit nach links
-	and R8, #0x0F ; binaeres UND mit 0Fh
-	orr R8, #0x05 ; binaeres ODER mit 05h
-	add R8, #2 ; Increment
+	;ldr R8, =0x54 ; Lade 54h in Register
+	;add R8, #1 ; Inkrementiere Register
+	;lsl R8, #1 ; Logisches Schieben um ein Bit nach links
+	;and R8, #0x0F ; binaeres UND mit 0Fh
+	;orr R8, #0x05 ; binaeres ODER mit 05h
+	;add R8, #2 ; Increment
 
-
+    mov R9, #0 ; Clear Register
+    ldr R10, =0x80000000
+	adds R11, R10, R9 ; Addiere 8000:0000h zum Register
+	adds R11, R11, R10 ; Addiere 8000:0000h zum Register
+	adcs R11, R11, R10 ; Addiere 8000:0000h zum Register
+	adcs R11, R11, R10 ; Addiere 8000:0000h zum Register
+	adds R11, R11, R10 ; Addiere 8000:0000h zum Register
 
    ; wiederholter Anwendungscode
 
